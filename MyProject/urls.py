f2331183aa236a4028fba6c8dfca826e3d10008e
Sandesh-Pol <sandeshpol12096@gmail.com>
2024-06-08 12:50:40 +0530

    url

--- MyProject/urls.py
+++ MyProject/urls.py
@@ -16,13 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.conf import settings
-from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('LoginApp.urls')),
-    path('home', include('event_card.urls')),
+    path('home/', include('event_card.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- event_card/urls.py
+++ event_card/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('cardview', views.index_view, name='index_view'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
