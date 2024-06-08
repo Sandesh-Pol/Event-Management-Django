85f6f41798973ff4f3ddc7b671600bf1736ea0d6
Sandesh-Pol <sandeshpol12096@gmail.com>
2024-06-08 14:47:51 +0530

    done

--- event_card/urls.py
+++ event_card/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('cardview/', views.index_view, name='index_view'),
     path('like/<int:event_id>/', views.like_event, name='like_event'),
